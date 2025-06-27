f2831d4de7378c19306541c167de22b5a6a39228
teovlt <teo.villet2@gmail.com>
2025-06-27 11:14:11 +0200

    Create categoryModel

---
+++ server/src/models/categoryModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Category = mongoose.model("Category", CategorySchema);
