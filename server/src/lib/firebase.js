11ced65f35062166bb6814911d90f5b0b0caa0e7
VILLET Téo <97348025+teovlt@users.noreply.github.com>
2025-07-07 14:55:39 +0200

    Update firebase.js

--- server/src/lib/firebase.js
+++ server/src/lib/firebase.js
@@ -7,7 +7,7 @@ admin.initializeApp({
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
-  storageBucket: "process.env.FIREBASE_BUCKET_NAME",
+  storageBucket: process.env.FIREBASE_BUCKET_NAME,
 });
 
 export const bucket = getStorage().bucket();
