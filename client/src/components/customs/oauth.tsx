5f44a439a8d1bb9857645273daee5d3cce10ba8a
VILLET Téo <97348025+teovlt@users.noreply.github.com>
2025-07-07 15:13:50 +0200

    res -> not response

--- client/src/components/customs/oauth.tsx
+++ client/src/components/customs/oauth.tsx
@@ -33,7 +33,7 @@ export function OAuth({ message }: OauthProps) {
 
       try {
         const res = await axiosConfig.post("/auth/login/google", userData);
-        localStorage.setItem("accessTokenInsightHub", response.data.accessToken);
+        localStorage.setItem("accessTokenInsightHub", res.data.accessToken);
         toast.success(t(res.data.message));
         setAuthUser(res.data.user);
         navigate("/");
